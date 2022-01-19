6b72ae99b556b1ffb821813c26a1c24227f851c7
Azka <azkahar@gmail.com>
2022-01-19 15:21:53 +0700

    Menghapus email di halaman about/file about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,7 +4,6 @@
 @section('container')
     <h1>Halaman about</h1>
     <p>{{ $name }}</p>
-    <p>{{ $email }}</p>
     <img src="img/{{ $gambar }}" alt="{{ $name }}" width="300px">    
     <p class="mt-3">Just a student who want to be a software engineer/programmer</p>
 @endsection
