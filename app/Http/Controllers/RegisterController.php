1ac20a01f79f5234d85f9d057d85df0a90da8665
Azka <azkahar@gmail.com>
2022-01-19 13:46:45 +0700

    Memperbaiki kata di RegisterController.php

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -29,6 +29,6 @@ class RegisterController extends Controller {
 
         // $request->session()->flash('success', 'Registration successfull! Please login');
 
-        return redirect('/login')->with('success', 'Registration successfull! Please login');
+        return redirect('/login')->with('success', 'Registration successfully! Please login');
     }
 }
