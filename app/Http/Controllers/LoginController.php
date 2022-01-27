bea74668130faf89c9ed0bddef61c8d52c176b32
Azka <azkahar@gmail.com>
2022-01-27 14:12:04 +0700

    nothing

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -23,7 +23,7 @@ class LoginController extends Controller {
         if(Auth::attempt($credentials)){
             $request->session()->regenerate();
 
-            return redirect()->intended('/dashboard');
+            return redirect()->intended('/dashboard')->with('success', 'Login success');
         }
 
         return back()->with('loginError', 'Login failed!');
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,6 +4,13 @@
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Welcome, {{ auth()->user()->name }}</h1>
     </div>
+
+    @if (session()->has('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
 {{-- 
     @foreach ($posts as $post)
         <p>{{ $post->title }}</p>
